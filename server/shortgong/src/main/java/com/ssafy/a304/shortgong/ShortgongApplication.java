d3eb145d6d3fd1c7fb10305d5e0925525232fb50
LeeSpect <wewqwew153@gmail.com>
2024-10-28 16:10:32 +0900

    Chore: back 프로젝트 init

---
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/ShortgongApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.a304.shortgong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortgongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortgongApplication.class, args);
+	}
+
+}
---
+++ server/shortgong/src/test/java/com/ssafy/a304/shortgong/ShortgongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a304.shortgong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortgongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
