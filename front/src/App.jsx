c7176d0dfa15fcf8e129f7b710a6bf8ea70e3ed5
정기영 <wjdrldud98@gmail.com>
2024-10-31 17:33:09 +0900

    Feat: icon 추가

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import icon from './assets/images/icon.png'
+
+function App() {
+  return (
+    <div>
+      <img src={icon} alt="App Icon" />
+    </div>
+  )
+}
+
+export default App
