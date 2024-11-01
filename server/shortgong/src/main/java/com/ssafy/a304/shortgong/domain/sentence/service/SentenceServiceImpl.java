95562f1dd91a9c425db3663743b60aca7d02ff3a
LeeSpect <wewqwew153@gmail.com>
2024-11-01 15:54:30 +0900

    Feat: SummaryService에 selectSummaryById 메서드 구현(S11P31A304-482)

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceServiceImpl.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceServiceImpl.java
@@ -3,7 +3,6 @@ package com.ssafy.a304.shortgong.domain.sentence.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ssafy.a304.shortgong.domain.sentence.model.dto.response.SentenceWithOutSummaryResponse;
 import com.ssafy.a304.shortgong.domain.sentence.repository.SentenceRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,11 +14,4 @@ public class SentenceServiceImpl implements SentenceService {
 
 	private final SentenceRepository sentenceRepository;
 
-	@Override
-	public SentenceWithOutSummaryResponse getSentence(Long sentenceId) throws Exception {
-
-		return SentenceWithOutSummaryResponse.from(
-			sentenceRepository.findById(sentenceId).orElseThrow(() -> new Exception(
-				"해당 문장이 존재하지 않습니다.")));
-	}
 }
--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/summary/service/SummaryServiceImpl.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/summary/service/SummaryServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssafy.a304.shortgong.domain.summary.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.a304.shortgong.domain.summary.model.entity.Summary;
 import com.ssafy.a304.shortgong.domain.summary.repository.SummaryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +15,11 @@ public class SummaryServiceImpl implements SummaryService {
 
 	private final SummaryRepository summaryRepository;
 
+	@Override
+	public Summary selectSummaryById(Long id) {
+
+		return summaryRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 id의 요약 정보가 존재하지 않습니다."));
+
+	}
 }
