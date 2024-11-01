be5a7d164b932c6718c7eb889c5cba2fdde87b28
LeeSpect <wewqwew153@gmail.com>
2024-11-01 16:06:35 +0900

    Feat: SentenceRepository에 SummaryId로 List<Sentence> 반환하는 findAllBySummary_Id 구현(S11P31A304-482)

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/repository/SentenceRepository.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/repository/SentenceRepository.java
@@ -1,9 +1,13 @@
 package com.ssafy.a304.shortgong.domain.sentence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.a304.shortgong.domain.sentence.model.entity.Sentence;
 
 public interface SentenceRepository extends JpaRepository<Sentence, Long> {
+
+	List<Sentence> findAllBySummary_Id(Long summaryId);
 
 }
