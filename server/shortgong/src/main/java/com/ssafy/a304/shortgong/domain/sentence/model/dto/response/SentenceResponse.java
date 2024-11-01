32552ccbab41c94860e3e853d9c6cdf20aae80cf
LeeSpect <wewqwew153@gmail.com>
2024-11-01 13:27:06 +0900

    Feat: SentenceResponse Dto 생성 SentenceResponse는 Repository에서 Sentence Entity를 가져온 후, Service에서 그 값들을 그대로 Dto에 담아 생성하는 용도

---
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/model/dto/response/SentenceResponse.java
@@ -0,0 +1,34 @@
+package com.ssafy.a304.shortgong.domain.sentence.model.dto.response;
+
+import com.ssafy.a304.shortgong.domain.sentence.model.entity.Sentence;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SentenceResponse {
+
+	private Long sentenceId;
+
+	private String sentenceContent;
+
+	private String voiceFileName;
+
+	private Integer order;
+
+	private String createdAt;
+
+	private Boolean openStatus;
+
+	public static SentenceResponse from(Sentence sentence) {
+		return SentenceResponse.builder()
+			.sentenceId(sentence.getSentenceId())
+			.sentenceContent(sentence.getSentenceContent())
+			.voiceFileName(sentence.getVoiceFileName())
+			.order(sentence.getOrder())
+			.createdAt(sentence.getCreatedAt().toString())
+			.openStatus(sentence.getOpenStatus())
+			.build();
+	}
+}
