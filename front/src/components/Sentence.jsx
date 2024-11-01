c3d07be9e8ff9ffbe130084e06cbcf6baf3b1c96
정기영 <wjdrldud98@gmail.com>
2024-11-01 16:30:00 +0900

    Design: 문장 컴포넌트 디자인 추가

--- front/src/components/Sentence.jsx
+++ front/src/components/Sentence.jsx
@@ -1,8 +1,35 @@
-import useLongPress from '../hooks/useLongPress'
 import React from 'react'
+import styled from '@emotion/styled'
+import useLongPress from '../hooks/useLongPress'
+import { colors } from '../assets/styles/colors'
 
-export default function Sentence({ text, onShortPress, onLongPress }) {
+const Container = styled.div`
+  width: 100%;
+  opcaity: 0.6;
+  ${(props) => {
+    if (props.status === 'hidden') {
+      return `
+        opacity: 0.3;
+        text-decoration: line-through;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      `
+    } else if (props.status === 'playing') {
+      return `
+        color: ${colors.primary};
+        opacity: 1;
+      `
+    }
+  }}
+`
+
+export default function Sentence({ text, status, onShortPress, onLongPress }) {
   const { ...longPressHandler } = useLongPress(onLongPress, onShortPress)
 
-  return <p {...longPressHandler}>{text}</p>
+  return (
+    <Container status={status} {...longPressHandler}>
+      {text}
+    </Container>
+  )
 }
