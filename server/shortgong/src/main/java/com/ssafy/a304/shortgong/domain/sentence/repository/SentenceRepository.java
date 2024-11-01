6c87b66e5c21231f9abaade888402796c70bb6d2
LeeSpect <wewqwew153@gmail.com>
2024-11-01 13:07:30 +0900

    Feat: SentenceRepository 구현(S11P31A304-482)

---
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/repository/SentenceRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.a304.shortgong.domain.sentence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.a304.shortgong.domain.sentence.model.entity.Sentence;
+
+public interface SentenceRepository extends JpaRepository<Sentence, Long> {
+}
