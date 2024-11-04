622d6e88f80051d275c1010b4bf0c30a14bd1575
jwun95 <jwun95@naver.com>
2024-11-04 17:43:00 +0900

    Feat: BackButtonTitle에서 Routes별로 제목 설정 TitleLayout과 Upload 페이지 CSS 작업

---
+++ front/src/routes.js
@@ -0,0 +1,10 @@
+// src/routes.js
+import React, { lazy } from 'react';
+
+const Upload = lazy(() => import('./pages/upload'));
+
+const routes = [
+  { path: '/upload', element: <Upload />, title: '자료 업로드' },
+];
+
+export default routes;
