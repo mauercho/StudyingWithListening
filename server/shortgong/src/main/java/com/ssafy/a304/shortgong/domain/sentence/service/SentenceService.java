f079b6953e51cd42a9fb61e2769eb23b9e4ce941
정재영 <hanol98@naver.com>
2024-11-05 16:06:56 +0900

    Remove: throws Exception 문 삭제

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceService.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceService.java
@@ -7,12 +7,14 @@ import com.ssafy.a304.shortgong.domain.sentence.model.entity.Sentence;
 
 public interface SentenceService {
 
-	Sentence selectSentenceById(Long id) throws Exception;
+	void addSentenceVoice(Sentence sentence);
+
 	Sentence selectSentenceById(Long id);
 
 	List<Sentence> selectAllSentenceBySummaryId(Long summaryId);
 
-	String convertSentenceListToString(List<Sentence> sentenceList) throws Exception;
+	List<SentenceResponse> searchAllSentenceResponseBySummaryId(Long summaryId);
+
 	String convertSentenceListToString(List<Sentence> sentenceList);
 
 	SentencesCreateResponse updateSentenceWithGPTUsingBulk(Long sentenceId, String GPTResponse) throws Exception;
