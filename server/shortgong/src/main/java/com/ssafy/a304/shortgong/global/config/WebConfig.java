87136778a44133663a4db39c1bfa668b7f4487a6
dongyeon <uuas5866@naver.com>
2024-11-06 10:57:56 +0900

    Fix: WebConfig Swagger api 접두사 제거 수정

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/config/WebConfig.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/config/WebConfig.java
@@ -9,6 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
-		configurer.addPathPrefix("api", c -> true);
+
+		// 모든 경로에 "api" 접두사를 추가하되, Swagger 경로는 제외
+		configurer.addPathPrefix("api", clazz -> {
+			// Swagger 관련 클래스들을 제외
+			String packageName = clazz.getPackageName();
+			return !packageName.contains("springdoc");
+		});
+		
 	}
 }
