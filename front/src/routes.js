008c2778e847365b1b04ff5fcb166c2d66782735
정기영 <wjdrldud98@gmail.com>
2024-11-06 10:36:34 +0900

    Feat: 로딩 컴포넌트 (S11P31A304-536)

--- front/src/routes.js
+++ front/src/routes.js
@@ -1,10 +1,8 @@
 // src/routes.js
-import React, { lazy } from 'react';
+import React, { lazy } from 'react'
 
-const Upload = lazy(() => import('./pages/upload'));
+const Upload = lazy(() => import('./pages/upload'))
 
-const routes = [
-  { path: '/upload', element: <Upload />, title: '자료 업로드' },
-];
+const routes = [{ path: '/upload', element: <Upload />, title: '자료 업로드' }]
 
-export default routes;
+export default routes
