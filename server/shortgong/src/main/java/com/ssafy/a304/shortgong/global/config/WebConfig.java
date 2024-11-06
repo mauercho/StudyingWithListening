245c71efb15084cb36c04b568c7bdee5fb3a2f85
dongyeon <uuas5866@naver.com>
2024-11-06 11:18:39 +0900

    Feat: Cors 설정 및 swagger url 설정 추가

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/config/WebConfig.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.a304.shortgong.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
 			String packageName = clazz.getPackageName();
 			return !packageName.contains("springdoc");
 		});
-		
+
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry.addMapping("/**")
+			.allowedOrigins("*")  // 모든 출처 허용
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
 	}
 }
