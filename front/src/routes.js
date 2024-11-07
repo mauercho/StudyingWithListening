e15313c2a747bbd2cb1f2d957e360b768f57b467
jwun95 <jwun95@naver.com>
2024-11-07 11:22:16 +0900

    Feat: List Page 구현

--- front/src/routes.js
+++ front/src/routes.js
@@ -2,10 +2,12 @@
 import React, { lazy } from 'react'
 
 const Upload = lazy(() => import('./pages/upload'))
+const List = lazy(() => import('./pages/list'))
 const Detail = lazy(() => import('./pages/detail'))
 
 const routes = [
   { path: '/upload', element: <Upload />, title: '자료 업로드' },
+  { path: '/list', element: <List />, title: '학습 목록' },
   { path: '/detail', element: <Detail />, title: '상세 페이지' },
 ]
 
