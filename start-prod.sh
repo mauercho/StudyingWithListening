c2a58806043d73b8518e96cb8cb99248100e28bf
dongyeon <uuas5866@naver.com>
2024-11-07 14:29:52 +0900

    Feat: python server Dockerfile 추가

--- start-prod.sh
+++ start-prod.sh
@@ -1,10 +1,14 @@
 docker-compose -f docker-compose-prod.yml down
+docker-compose -f docker-compose-fastapi.yml down
 
 docker rmi -f $(docker images shortgong_backend:latest -q) || true
 docker rmi -f $(docker images shortgong_frontend:latest -q) || true
+docker rmi -f $(docker images shortgong_crawling:latest -q) || true
 
 docker-compose -f docker-compose-prod.yml pull
+docker-compose -f docker-compose-fastapi.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-prod.yml up --build -d
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-fastapi.yml up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
