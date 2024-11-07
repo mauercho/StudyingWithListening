9075d3995ee2970635b6f055349ec0c861ac85d7
dongyeon <uuas5866@naver.com>
2024-11-07 10:46:29 +0900

    Fix: Cloud Front private key 설정 수정

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/util/CloudFrontSignedUrlUtil.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/global/util/CloudFrontSignedUrlUtil.java
@@ -1,11 +1,14 @@
 package com.ssafy.a304.shortgong.global.util;
 
-import java.security.PrivateKey;
+import java.io.File;
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.cloudfront.CloudFrontUrlSigner;
+import com.amazonaws.services.cloudfront.util.SignerUtils;
 import com.ssafy.a304.shortgong.global.config.CloudFrontConfig;
 
 import lombok.RequiredArgsConstructor;
@@ -16,16 +19,16 @@ public class CloudFrontSignedUrlUtil {
 
 	private final CloudFrontConfig cloudFrontConfig;
 
-	private final PrivateKey cloudFrontPrivateKey;
-
-	public String generateSignedUrl(String relativeFilePath) {
+	public String generateSignedUrl(String relativeFilePath) throws InvalidKeySpecException, IOException {
 
 		Date expiration = new Date(System.currentTimeMillis() + (long)60 * 1000);
 
 		return CloudFrontUrlSigner.getSignedURLWithCannedPolicy(
-			cloudFrontConfig.getCloudFrontUrl() + "/" + relativeFilePath,
+			SignerUtils.Protocol.https,
+			cloudFrontConfig.getCloudFrontUrl(),
+			new File(cloudFrontConfig.getPrivateKeyFilePath()),
+			relativeFilePath,
 			cloudFrontConfig.getKeyPairId(),
-			cloudFrontPrivateKey,
 			expiration
 		);
 	}
