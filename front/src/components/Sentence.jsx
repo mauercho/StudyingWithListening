3bd60787ca3942c18f053d35837dbb0ff921c268
정기영 <wjdrldud98@gmail.com>
2024-11-12 16:31:30 +0900

    Feat: 문장 진행 중 아이콘 추가 및 TODO 추가

--- front/src/components/Sentence.jsx
+++ front/src/components/Sentence.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import styled from '@emotion/styled'
+import { FaHeadphones } from 'react-icons/fa'
 
 import useLongPress from '../hooks/useLongPress'
 
 const Container = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
   width: 100%;
   cursor: pointer;
   padding: 10px;
@@ -17,6 +22,7 @@ const Container = styled.li`
 `
 
 const Text = styled.p`
+  width: 100%;
   opacity: 0.6;
   font-size: 16px;
   font-weight: ${({ theme }) => theme.font.weight.light};
@@ -29,21 +35,22 @@ const Text = styled.p`
       overflow: hidden;
       text-overflow: ellipsis;
     `}
-
-  ${({ theme, status }) =>
-    status === 'playing' &&
+  ${({ theme }) =>
+    false && // '현재 진행중인 id가 저장된 id와 같다면 == 진행상태면'
     `
       color: ${theme.color.primary_dark};
       opacity: 1;
       font-weight: ${theme.font.weight.regular};
     `}
 `
+// Q 코드 { color: ${theme.color.secondary} } A 코드 { color: ${theme.color.primary_dark} }
 
 export default function Sentence({ text, status, onShortPress, onLongPress }) {
   const { ...longPressHandler } = useLongPress(onLongPress)
-
+  // TODO: player 코드랑 usePlayerStore 업데이트 되면 헤드폰 조건 변경해주기, API 업데이트 후 스타일 적용하기
   return (
     <Container onClick={onShortPress} {...longPressHandler}>
+      {false && <FaHeadphones size={16} />}
       <Text status={status}>{text}</Text>
     </Container>
   )
