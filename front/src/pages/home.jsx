42d6727cab9e376463e431b82e43296f0b58c040
정기영 <wjdrldud98@gmail.com>
2024-11-13 20:32:24 +0900

    Feat: 새 메인페이지 적용

--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styled from '@emotion/styled'
 
-import HomeMenu from '../components/HomeMenu'
 import Logo from '../assets/images/logo.svg'
 import { FaBookMedical, FaEarListen } from 'react-icons/fa6'
+
+import FileInput from '../components/FileInput'
+import summariesApi from '../api/summariesApi'
 
 const Container = styled.div`
   padding-top: 30px;
@@ -19,34 +22,74 @@ const Text = styled.p`
   font-weight: ${({ theme }) => theme.font.weight.regular};
 `
 
-const Menus = styled.ul`
+const SubTitle = styled.p`
   width: 100%;
+  font-size: ${({ theme }) => theme.font.size.xl};
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+`
+
+const ListContainer = styled.ul`
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
-  justify-content: center;
-  gap: 10px;
-  flex: 1;
+  margin-top: 10px;
+  width: 100%;
+  overflow-y: auto;
+  border-top: 1px solid ${({ theme }) => theme.color.primary_dark};
+`
+
+const Item = styled.li`
+  height: 72px;
+  padding: 0px 10px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  cursor: pointer;
+  box-sizing: border-box;
+  border-bottom: 1px solid ${({ theme }) => theme.color.primary_dark};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.grey};
+  }
 `
 
 export default function Home() {
+  const [list, setList] = useState([])
+  const navigate = useNavigate()
+
+  const handleLinkClick = (summariesId) => {
+    navigate(`/detail/${summariesId}`)
+  }
+
+  useEffect(() => {
+    const fetchSummaries = async () => {
+      try {
+        const data = await summariesApi.getSummaries()
+        console.log('1,2, 3,4,')
+        setList(data)
+      } catch (error) {
+        console.error('Error fetching user:', error)
+      }
+    }
+
+    fetchSummaries()
+  }, [])
+
   return (
     <Container>
       <img src={Logo} alt="logo.svg" width={133} height={72} />
-      <Text>이동중에도 멈추지 않는 공부</Text>
-      <Menus>
-        <HomeMenu
-          icon={<FaBookMedical size={24} />}
-          title="자료 업로드"
-          subTitle="학습할 자료를 업로드"
-          url="upload"
-        />
-        <HomeMenu
-          icon={<FaEarListen size={24} />}
-          title="학습 목록"
-          subTitle="학습 목록 보기"
-          url="list"
-        />
-      </Menus>
+      <Text>듣는 것만으로 암기가 되는 학습</Text>
+      <FileInput />
+      <SubTitle>이전 학습</SubTitle>
+      <ListContainer>
+        {list.map((v, index) => {
+          return (
+            <Item key={index} onClick={() => handleLinkClick(v.id)}>
+              {v.title ? v.title : '제목 없음'}
+            </Item>
+          )
+        })}
+      </ListContainer>
     </Container>
   )
 }
