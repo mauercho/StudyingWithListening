6638bf310bf4f814aab99710f4ada243d0969fd6
JaeY0ungJung <hanol5216@gmail.com>
2024-11-14 14:00:50 +0900

    Fix: 프롬프트 연결 및 Q 개수 3개로 제한

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/model/dto/response/QuestionAnswerResponse.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/model/dto/response/QuestionAnswerResponse.java
@@ -3,25 +3,25 @@ package com.ssafy.a304.shortgong.domain.sentence.model.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class QuestionAnswerResponse {
 
-	String point; // 스키마를 통한 지식 습득
+	private String point; // 스키마를 통한 지식 습득
+	private String question; // 스키마란 무엇이며, 새로운 지식을 습득하는 과정에서 어떤 역할을 하나요?
+	private String answerDetail; // 새로운 지식은 기존의 지식을 통해 습득됩니다. 사람은 자신이 이미 가지고 있는 지식을 기반으로 새로운 지식을 이해하고 해석하게 됩니다.
+	private String answerSimple;
+	private String answerNormal;
 
-	String question; // 스키마란 무엇이며, 새로운 지식을 습득하는 과정에서 어떤 역할을 하나요?
+	@Builder
+	QuestionAnswerResponse(String point, String question) {
 
-	String answer; // 새로운 지식은 기존의 지식을 통해 습득됩니다. 사람은 자신이 이미 가지고 있는 지식을 기반으로 새로운 지식을 이해하고 해석하게 됩니다.
-
-	public static QuestionAnswerResponse of(String point, String question, String answer) {
-
-		return QuestionAnswerResponse.builder()
-			.point(point)
-			.question(question)
-			.answer(answer)
-			.build();
+		this.point = point;
+		this.question = question;
 	}
-
 }
