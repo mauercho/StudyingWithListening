60adcbcc3bb6459b11b8c139fc5a53c53697751a
LeeSpect <wewqwew153@gmail.com>
2024-11-17 23:31:26 +0900

    Feat: Keyword만으로 TPQ와 SA, NA 반환하는 프롬프트 메서드 추가

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/test/controller/ClaudeTestController.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/test/controller/ClaudeTestController.java
@@ -1,7 +1,9 @@
 package com.ssafy.a304.shortgong.domain.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.a304.shortgong.domain.test.service.ClaudeTestService;
@@ -37,6 +39,19 @@ public class ClaudeTestController {
 	public void testImage() throws Exception {
 
 		claudeTestService.testImage();
+	}
+
+	@GetMapping("/keyword/TPQ")
+	public void testKeyword() {
+
+		claudeTestService.testKeyword();
+	}
+
+	@GetMapping("/keyword-answer")
+	public void testKeywordAnswer(@RequestParam(name = "T") String T, @RequestParam(name = "P") String P, @RequestParam(name = "Q") String Q,
+		@RequestParam(name = "keyword") String keyword) {
+
+		claudeTestService.testKeywordAnswer(T, P, Q, keyword);
 	}
 
 }
