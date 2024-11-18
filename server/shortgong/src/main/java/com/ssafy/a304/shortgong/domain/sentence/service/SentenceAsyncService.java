0f8dd34281bd5ecb61c4ebe17ecc130846194d91
JaeY0ungJung <hanol5216@gmail.com>
2024-11-19 01:01:28 +0900

    Fix: order 에러 수정

--- server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceAsyncService.java
+++ server/shortgong/src/main/java/com/ssafy/a304/shortgong/domain/sentence/service/SentenceAsyncService.java
@@ -1,7 +1,6 @@
 package com.ssafy.a304.shortgong.domain.sentence.service;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.scheduling.annotation.Async;
 
@@ -11,10 +10,9 @@ import com.ssafy.a304.shortgong.domain.summary.model.entity.Summary;
 public interface SentenceAsyncService {
 
 	@Async
-	CompletableFuture<Void> getAnswerAndVoices(QuestionResponse questionResponse, String text, Summary summary,
-		AtomicInteger orderCounter);
+	CompletableFuture<Void> getAnswerAndVoices(QuestionResponse questionResponse, String text, Summary summary);
 
 	@Async
-	CompletableFuture<Void> getAnswerAndVoicesByKeyword(QuestionResponse questionResponse, String text, Summary summary,
-		AtomicInteger orderCounter);
+	CompletableFuture<Void> getAnswerAndVoicesByKeyword(QuestionResponse questionResponse, String text,
+		Summary summary);
 }
